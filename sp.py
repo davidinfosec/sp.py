0f230ad84cc6dacd564967c179b97858687f79e2
David <87215831+davidinfosec@users.noreply.github.com>
2023-10-28 14:38:02 -0400

    Add files via upload

--- sp.py
+++ sp.py
@@ -7,8 +7,9 @@ import csv
 import datetime
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
+import requests
 
-# Try to read the client ID from client_id.txt
+# Initialize Spotify Client ID and Secret
 try:
     with open('./client_id.txt', 'r') as file:
         SPOTIPY_CLIENT_ID = file.read().strip()
@@ -17,7 +18,6 @@ except FileNotFoundError:
     with open('./client_id.txt', 'w') as file:
         file.write(SPOTIPY_CLIENT_ID)
 
-# Try to read the client secret from client_secret.txt
 try:
     with open('./client_secret.txt', 'r') as file:
         SPOTIPY_CLIENT_SECRET = file.read().strip()
@@ -32,7 +32,7 @@ api_cache = {}
 # Define a list to store recent searches
 recent_searches = []
 
-# Create a subdirectory for the CSV files
+# Define a subdirectory for CSV files
 current_date = datetime.datetime.now().strftime("%Y-%m")
 subdirectory = f"sp-{current_date}"
 
@@ -45,11 +45,11 @@ csv_file = f"{subdirectory}/sp-py-{datetime.datetime.now().strftime('%Y-%m-%d')}
 def initialize_csv(csv_file):
     if not os.path.exists(csv_file):
         headers = ["Type", "Playlist Name", "Search Term", "Spotify Link", "YouTube Link"]
-        with open(csv_file, mode='w', newline='') as file:
+        with open(csv_file, mode='a', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerow(headers)
 
-# Get YouTube link and song info with caching
+# Define a function to get YouTube link and song info with caching
 def get_youtube_link_cached(artist, track_name):
     query = f"{artist} {track_name} official video"
     if query in api_cache:
@@ -64,7 +64,7 @@ def get_youtube_link_cached(artist, track_name):
     api_cache[query] = result
     return result
 
-# Get Spotify link
+# Define a function to get Spotify link
 def get_spotify_link(artist, track_name):
     client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
     sp = Spotify(client_credentials_manager=client_credentials_manager)
@@ -79,23 +79,23 @@ def get_spotify_link(artist, track_name):
     else:
         return "No results found on Spotify."
 
-# Export recent searches to CSV
+# Define a function to export recent searches to CSV
 def export_recent_searches(csv_file):
     if recent_searches:
         if not os.path.exists(csv_file):
             initialize_csv(csv_file)
-        with open(csv_file, mode='a', newline='') as file:
+        with open(csv_file, mode='a', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             for entry in recent_searches:
                 writer.writerow(entry)
         print(f"Searches appended to {csv_file}")
         recent_searches.clear()
 
-# Clear the terminal screen
+# Define a function to clear the terminal screen
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-# Process a track
+# Define a function to process a track
 def process_track(track, playlist_name):
     artist = track['track']['artists'][0]['name']
     track_name = track['track']['name']
@@ -114,29 +114,11 @@ def process_track(track, playlist_name):
         print("\n\n")
         recent_searches.append(["Single", playlist_name, f"{artist} - {track_name}", spotify_link, youtube_link])
         export_recent_searches(csv_file)
-
-# Process an album for bulk search with threading
-""" def process_album(album_link):
-    client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
-    sp = Spotify(client_credentials_manager=client_credentials_manager)
-
-    # Extract album ID from the link
-    album_id = album_link.split("album/")[-1].split("?")[0]
-    results = sp.album_tracks(album_id)
-
-    total_tracks = len(results['items']) if 'items' in results else 0
-
-    if total_tracks > 0:
-        with ThreadPoolExecutor() as executor:
-            try:
-                list(tqdm(executor.map(process_track, results['items'], ["N/A"]*total_tracks, ["N/A"]*total_tracks), total=total_tracks, desc=f"Processing tracks in album", unit="track"))
-            except Exception as e:
-                print(f"Error processing tracks in album: {e}")
+        send_message_to_webhook(webhook_url, f"YouTube Link: {youtube_link}\n{song_info}\n\n")
     else:
-        print(f"No tracks found in album.") """
+        print(youtube_link)
 
-
-# Process a playlist for bulk search
+# Define a function to process a playlist for bulk search
 def process_playlist(playlist_link, playlist_name):
     client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
     sp = Spotify(client_credentials_manager=client_credentials_manager)
@@ -153,17 +135,49 @@ def process_playlist(playlist_link, playlist_name):
     else:
         print(f"No tracks found in playlist '{playlist_name}'.")
 
-# Main function
+# Define a function to manage webhook settings
+def manage_webhook():
+    global webhook_url
+    use_webhook = input("Do you want to use a webhook for notifications? (yes/no): ").lower()
+    if use_webhook == 'yes':
+        webhook_url = input("Enter your Discord webhook URL: ")
+        with open('./webhook_url.txt', 'w') as file:
+            file.write(webhook_url)
+    elif use_webhook == 'no':
+        webhook_url = None
+        if os.path.exists('./webhook_url.txt'):
+            os.remove('./webhook_url.txt')
+
+# Define a function to send a message to the webhook
+def send_message_to_webhook(webhook_url, message):
+    if webhook_url is not None:
+        payload = {'content': message}
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(webhook_url, json=payload, headers=headers)
+        response.raise_for_status()  # Raise an exception for bad response status codes
+
+
+# Define the main function
 def main():
+    global webhook_url
     sp = Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET))
     
-    # Re-create CSV file if it doesn't exist
     if not os.path.exists(csv_file):
         initialize_csv(csv_file)
+
+    manage_webhook()  # Ask user about webhook preference
 
     while True:
         clear_screen()
         print("Spotify to YouTube / YouTube Link Search Tool")
+        print(r'''
+   ___ _ __   _ __  _   _ 
+  / __| '_ \ | '_ \| | | |
+  \__ \ |_) || |_) | |_| |
+  |___/ .__(_) .__/ \__, |
+      | |    | |     __/ |
+      |_|    |_|    |___/ 
+        ''')
         user_input = input("Enter artist and track name (Artist,TrackName), Spotify link, 'cls' to clear screen, '--bulk PlaylistLink PlaylistName' to process a playlist, or 'quit' to exit (c to cancel): ")
 
         if user_input.lower() == 'quit':
@@ -230,6 +244,7 @@ def main():
             print("\n\n")
             recent_searches.append(["Single", "N/A", f"{artist} - {track_name}", spotify_link, youtube_link])
             export_recent_searches(csv_file)
+            send_message_to_webhook(webhook_url, f"YouTube Link: {youtube_link}\n{song_info}\n\n")
         else:
             print(youtube_link)
 
